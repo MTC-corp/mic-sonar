38cc6901825e01d241bd194c01c25c9572bb00cc
Henry Villavicencio <henry.villavicencio.dev@gmail.com>
2024-01-04 06:46:54 -0500

    add some bugs

--- App.js
+++ App.js
@@ -1,9 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+// Key paypal
+const KeyPaypal = "c4b977c0-9ec5-4ca3-a2af-cf1d32683016";
 
 export default function App() {
+  useEffect(() => {
+    console.log(KeyPaypal);
+  }, []);
+
+  const onPress = () => {
+    // DO something
+  };
+
   return (
     <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -13,8 +30,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
